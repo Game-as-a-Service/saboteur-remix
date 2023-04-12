5a48a944f1fe5bc91122fdc828fa50987d689e3f
kayac-chang <egg734631@gmail.com>
2023-04-12 21:05:54 +0800

    test: 💍 check position has been placed

--- apps/backend/src/board/logic/check-positions-has-been-placed.test.ts
+++ apps/backend/src/board/logic/check-positions-has-been-placed.test.ts
@@ -1,4 +1,9 @@
 import type { EventSource } from "~/models/event";
+import { PathCard } from "~/models/card";
+import { never } from "~/utils";
+import { PlacePathCardCommand } from "~/board/command";
+import { PathCardHasBeenPlacedEvent } from "~/board/event";
+import checkPositionsHasBeenPlaced from "./check-positions-has-been-placed";
 
 describe("check positions has been placed", () => {
   let source: EventSource;
@@ -15,9 +20,76 @@ describe("check positions has been placed", () => {
     source = { append, read };
   });
 
-  test.todo(`
+  test(`
       given:
+        an empty board
       when:
+        place path card command
       expect:
-    `);
+        should return place path card command
+    `, () =>
+    Promise.resolve(
+      checkPositionsHasBeenPlaced(
+        source,
+        PlacePathCardCommand({
+          card: PathCard.CONNECTED_CROSS,
+          position: [0, 0],
+        })
+      )
+    )
+      //
+      .then((result) =>
+        result.match(
+          (command) =>
+            expect(command).toStrictEqual(
+              PlacePathCardCommand({
+                card: PathCard.CONNECTED_CROSS,
+                position: [0, 0],
+              })
+            ),
+          never
+        )
+      ));
+
+  test(`
+      given:
+        a board with:
+          - start card at position (0, 0)
+      when:
+        place path card command with:
+          - cross card at position (0, 0)
+      expect:
+        should throw placement has been placed error
+    `, () =>
+    Promise.resolve()
+      .then(() =>
+        source.append(
+          PathCardHasBeenPlacedEvent({
+            card: PathCard.CONNECTED_CROSS,
+            position: [0, 0],
+          })
+        )
+      )
+      .then(() =>
+        checkPositionsHasBeenPlaced(
+          source,
+          PlacePathCardCommand({
+            card: PathCard.CONNECTED_CROSS,
+            position: [0, 0],
+          })
+        )
+      )
+      .then((result) =>
+        result.match(
+          never,
+          (error) =>
+            expect(error).toStrictEqual(
+              AggregateError([
+                `the path card ${PathCard.CONNECTED_CROSS} cannot be placed at position (0,0)`,
+                //
+              ])
+            )
+          //
+        )
+      ));
 });
