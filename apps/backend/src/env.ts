848a7a0500a0f12a0146684279591dbe21c3d957
Hello <egg734631@gmail.com>
2023-04-15 00:23:33 +0800

    feat: 🎸 redis streams adapter (#21)
    * feat: 🎸 redis streams adapter
    
    * feat: 🎸 redis url
    
    * ci: 🎡 create temp redis for preview

---
+++ apps/backend/src/env.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export default z
+  .object({
+    PORT: z.coerce.number({
+      required_error: "ENV PORT is required",
+    }),
+    REDIS_URL: z.string({
+      required_error: "ENV REDIS_URL is required",
+    }),
+    CLIENT_PORT: z.coerce.number().optional(),
+    CORS_ORIGIN: z
+      .string()
+      .optional()
+      .default("")
+      .transform((val) => val.split(",")),
+  })
+  .parse(process.env);
--- apps/backend/src/io/redis.ts
+++ apps/backend/src/io/redis.ts
@@ -1,20 +1,24 @@
 import { createClient } from "redis";
 import { v4 as uuid } from "uuid";
+import { Logger } from "@nestjs/common";
+import env from "~/env";
+
+const logger = new Logger("Redis (redis.ts)");
 
 async function redis(id = uuid()) {
-  const client = createClient();
+  const client = createClient({ url: env.REDIS_URL });
 
   client.once("connect", () =>
-    console.log(`Initiating connection:${id} to the redis server`)
+    logger.log(`Initiating connection:${id} to the redis server`)
   );
   client.once("ready", () =>
-    console.log(`Connection:${id} to the redis server is ready`)
+    logger.log(`Connection:${id} to the redis server is ready`)
   );
   client.once("end", () =>
-    console.log(`Connection:${id} to the redis server ended`)
+    logger.log(`Connection:${id} to the redis server ended`)
   );
   client.once("error", (error) =>
-    console.error(`Connection:${id} to the redis server error`, error)
+    logger.error(`Connection:${id} to the redis server error`, error)
   );
 
   await client.connect();
