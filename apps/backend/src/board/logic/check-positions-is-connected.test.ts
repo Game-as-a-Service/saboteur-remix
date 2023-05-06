510c51468629573639e8151c7e739190f90d00ae
Ching Chang <nikki12345001@gmail.com>
2023-05-06 14:19:27 -0700

    Fix: check connect to neighbor logic
    - when check connectivity relationships, should use xnor instead of and
    - fix vector diff
    - add unit tests
      - test isConnectNeighbor
      - test getNeighbors

---
+++ apps/backend/src/board/logic/check-positions-is-connected.test.ts
@@ -0,0 +1,53 @@
+import { PathCard } from "~/models/card";
+import { never } from "~/utils";
+import { Position } from "./get-available-positions";
+import { getNeighbors, isConnectNeighbor } from "./check-positions-is-connected";
+import { Placement } from "~/models/placement";
+
+describe("check positions is connected", () => {
+  let board: Placement[];
+
+  beforeEach(() => {
+    board = [
+      { position: [0, 0], card: PathCard.START },
+      { position: [0, 1], card: PathCard.CONNECTED_BOTTOM_RIGHT },
+      { position: [1, 1], card: PathCard.CONNECTED_TOP_LEFT_RIGHT },
+      { position: [-1, 0], card: PathCard.DEADEND_TOP_BOTTOM_RIGHT },
+    ];
+  });
+
+  it.each<[Position, Position[]]>([
+    [[1,1], [[1,2], [1,0], [2,1], [0,1]]],
+    [[-1,-1], [[-1,0], [-1,-2], [0,-1], [-2,-1]]],
+  ])(`
+      [getNeighbors]
+      given:
+        position
+      expect:
+        should return positions of its top, right, bottom, left neighbors
+    `, (position: Position, expected: Position[]) => {
+      expect(getNeighbors(position)).toIncludeSameMembers(expected);
+    });
+
+  it.each<[Placement, boolean]>([
+    [{ position: [1, 0], card: PathCard.CONNECTED_BOTTOM_RIGHT }, false],
+    [{ position: [1, 0], card: PathCard.CONNECTED_LEFT_RIGHT }, true],
+    [{ position: [1, 0], card: PathCard.CONNECTED_CROSS }, false],
+    [{ position: [1, 0], card: PathCard.CONNECTED_TOP_LEFT_RIGHT }, false],
+    [{ position: [1, 0], card: PathCard.DEADEND_LEFT }, true],
+    [{ position: [1, 0], card: PathCard.DEADEND_BOTTOM_LEFT }, true],
+    [{ position: [-1, 1], card: PathCard.DEADEND_LEFT }, false],
+    [{ position: [-1, 1], card: PathCard.DEADEND_BOTTOM_LEFT }, true],
+    [{ position: [-1, 1], card: PathCard.DEADEND_BOTTOM_RIGHT }, false],
+    [{ position: [-1, 1], card: PathCard.DEADEND_BOTTOM }, true],
+  ])(`
+      [isConnectNeighbor]
+      preset: board
+      given:
+        a placement %j
+      expect:
+        is placement connected to neighbors on current board: %j
+    `, (y: Placement, expected: boolean) => {
+      expect(isConnectNeighbor(board)(y)).toStrictEqual(expected);
+    });
+});
