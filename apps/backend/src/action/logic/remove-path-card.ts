7d4813f90ee8da5fe4c8f2170e960ae596f0c21b
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2023-05-07 12:11:02 +0800

    Feature/remove path card logic and test (#65)
    * feat: add remove path card logic
    
    * feat: 👔 update remove path card logic
    
    * test: ✅ add remove path card unit test
    
    1. Add remove path card unit test
    2. Modify type
    
    Close: #57
    
    * refactor: ♻️ Refactor code
    1. Refactor code to improve the code readability
    2.  Remove TODO comments
    Close: #57

--- apps/backend/src/action/logic/remove-path-card.ts
+++ apps/backend/src/action/logic/remove-path-card.ts
@@ -1,14 +1,84 @@
-import { ResultAsync, errAsync } from "neverthrow";
+import { ResultAsync, ok, err } from "neverthrow";
 import { EventSource } from "~/models/event";
 import { RockfallCommand } from "~/action/command";
-import { PathCardHasBeenRemovedEvent, Event } from "~/action/event";
+import { PathCardHasBeenRemovedEvent } from "~/action/event";
+import type { BoardEvent } from "~/board/event";
+import type { RepositoryReadError } from "~/board/logic/get-current-placements";
+import getCurrentPlacements from "~/board/logic/get-current-placements";
+import { Placement } from "~/models/placement";
+import { flow, pipe } from "fp-ts/lib/function";
+import { prop, error, always } from "~/utils";
+import { PathCard } from "~/models/card";
+import * as Vec from "~/models/vec";
+import * as Array from "fp-ts/Array";
+import * as Either from "fp-ts/Either";
+
+export type BoardCardEvent = BoardEvent | PathCardHasBeenRemovedEvent;
+
+const TargetCannotBeRemovedError = error("TargetCannotBeRemovedError");
+const RepositoryWriteError = error("RepositoryWriteError");
+export type RepositoryWriteError = ReturnType<typeof RepositoryWriteError>;
+export type TargetCannotBeRemovedError = ReturnType<
+  typeof TargetCannotBeRemovedError
+>;
+export type RemovePathCardError =
+  | TargetCannotBeRemovedError
+  | RepositoryWriteError
+  | RepositoryReadError;
 
 export interface RemovePathCard {
-  (source: EventSource<Event>, command: RockfallCommand): ResultAsync<
+  (source: EventSource<BoardCardEvent>, command: RockfallCommand): ResultAsync<
     PathCardHasBeenRemovedEvent,
-    Error
+    RemovePathCardError
   >;
 }
+
+const NonRemovablePathCard = Object.freeze([
+  PathCard.START,
+  PathCard.GOAL_COAL_BOTTOM_LEFT,
+  PathCard.GOAL_COAL_BOTTOM_RIGHT,
+  PathCard.GOAL_GOLD,
+]);
+
+const isRemovablePathCard = Either.fromPredicate<
+  Placement,
+  TargetCannotBeRemovedError
+>(
+  (placement) => !NonRemovablePathCard.includes(placement.card),
+  (placement) =>
+    TargetCannotBeRemovedError(
+      `the path card ${placement.card} cannot be removed`
+    )
+);
+
+const findPlacementByPosition = (command: RockfallCommand) =>
+  Array.findFirst<Placement>(
+    flow(prop("position"), Vec.eq(command.data.position))
+  );
+
+const checkPathCardHasBeenPlaced =
+  (command: RockfallCommand) => (board: Placement[]) =>
+    pipe(
+      board,
+      findPlacementByPosition(command),
+      Either.fromOption(() =>
+        TargetCannotBeRemovedError("empty placement cannot be removed")
+      ),
+      Either.chain(isRemovablePathCard),
+      Either.matchW(err, always(ok(command)))
+    );
+
+const appendEventToEventSource =
+  (source: EventSource<BoardCardEvent>, command: RockfallCommand) => () =>
+    pipe(
+      PathCardHasBeenRemovedEvent(command.data),
+      (event) =>
+        ResultAsync.fromPromise(
+          source.append(event),
+          always(RepositoryWriteError("failed to write event to repository"))
+        ).map(() => event)
+      //
+    );
 
 /**
  * *description*
@@ -17,11 +87,10 @@ export interface RemovePathCard {
  * *param* source - event source
  * *param* command - rockfall command
  */
-export const removePathCard: RemovePathCard = (source, command) => {
-  //@todo: read from event source
-  //@todo: check path card has been placed
-  //@todo: append event to event source
-  return errAsync(new Error("not implemented"));
-};
+
+export const removePathCard: RemovePathCard = (source, command) =>
+  getCurrentPlacements(source as EventSource<BoardEvent>)
+    .andThen(checkPathCardHasBeenPlaced(command))
+    .andThen(appendEventToEventSource(source, command));
 
 export default removePathCard;
