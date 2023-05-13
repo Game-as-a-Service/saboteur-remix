6fc90587abb0b9f4c4a68f88ef09e1313a03eea7
Dluter <tom230023@gmail.com>
2023-05-14 00:48:10 +0800

    feat: add get current player hand test

--- apps/backend/src/action/event/index.ts
+++ apps/backend/src/action/event/index.ts
@@ -3,13 +3,15 @@ import type { BrokenToolHasBeenPlacedEvent } from "./broken-tool-has-been-placed
 import type { BrokenToolHasBeenRemovedEvent } from "./broken-tool-has-been-removed.event";
 import type { GoalCardHasBeenRevealEvent } from "./goal-card-has-been-reveal.event";
 import type { TurnHasBeenPassedEvent } from "./turn-has-been-passed.event";
+import { PlayerHadHandLeftEvent } from "./player-had-hand-left";
 
 export type Event =
   | PathCardHasBeenRemovedEvent
   | BrokenToolHasBeenPlacedEvent
   | BrokenToolHasBeenRemovedEvent
   | GoalCardHasBeenRevealEvent
-  | TurnHasBeenPassedEvent;
+  | TurnHasBeenPassedEvent
+  | PlayerHadHandLeftEvent;
 
 export * from "./path-card-has-been-removed.event";
 export * from "./broken-tool-has-been-placed.event";
