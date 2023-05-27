eeeaea5798cd5ea9ecea712b927f17b5fe4bf1e7
kayac-chang <egg734631@gmail.com>
2023-05-27 22:02:13 +0800

    feat: 🎸 add board player slice

--- apps/frontend/app/routes/game/store/board.slice.ts
+++ apps/frontend/app/routes/game/store/board.slice.ts
@@ -1,9 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import type { Placement, Vec2 } from "@packages/domain";
+import type { RootState } from ".";
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import store from ".";
+
+const hash = (position: Vec2) => position.join(",");
+const adapter = createEntityAdapter<Placement>({
+  selectId: (placement) => hash(placement.position),
+});
 
 export default createSlice({
   name: "board",
-  initialState: {
-    board: [],
+  initialState: adapter.getInitialState(),
+  reducers: {
+    add: adapter.addOne,
+    remove: adapter.removeOne,
+    upsert: adapter.upsertOne,
+    reset: adapter.getInitialState,
   },
-  reducers: {},
 });
+
+// selectors
+const selectors = adapter.getSelectors<RootState>((state) => state.board);
+export const selectBoard = () => selectors.selectAll(store.getState());
+export const selectBoardByPosition = (position: Vec2) =>
+  selectors.selectById(store.getState(), hash(position));
--- apps/frontend/app/routes/game/store/index.ts
+++ apps/frontend/app/routes/game/store/index.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import boardSlice from "./board.slice";
+import playersSlice from "./players.slice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     board: boardSlice.reducer,
+    players: playersSlice.reducer,
   },
 });
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
