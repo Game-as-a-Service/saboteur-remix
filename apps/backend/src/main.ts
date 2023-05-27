3bad5be8f8bfa8d754d6b2731e8536f145120319
Hello <egg734631@gmail.com>
2023-05-27 21:26:02 +0800

    fix: 🐛 socket cors support (#92)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,31 +8,33 @@ import cors from "@fastify/cors";
 const log = logger({ tag: `main` });
 const server = fastify();
 
+const origin = [
+  ...(env.CLIENT_PORT
+    ? [
+        `http://localhost:${env.CLIENT_PORT}`,
+        // to match http://192.168.1.(\d):PORT
+        RegExp(`/^http:\/\/192\.168\.1\.([1-9][1-9]\d):${env.CLIENT_PORT}$/`),
+      ]
+    : []),
+  ...env.CORS_ORIGIN,
+];
+
 // support cors
-server.register(cors, {
-  origin: [
-    ...(env.CLIENT_PORT
-      ? [
-          `http://localhost:${env.CLIENT_PORT}`,
-          // to match http://192.168.1.(\d):PORT
-          RegExp(`/^http:\/\/192\.168\.1\.([1-9][1-9]\d):${env.CLIENT_PORT}$/`),
-        ]
-      : []),
-    ...env.CORS_ORIGIN,
-  ],
-});
+server.register(cors, { origin });
 
 // health check
 server.get("/health", (_, res) => res.send("ok"));
 
 // support socket.io
-server.register(fastifyIO);
+server.register(fastifyIO, { cors: { origin } });
 server.ready().then(() => {
   server.io.on("connection", gateway);
 });
 
-server.listen({ port: env.PORT, host: "0.0.0.0" }, () =>
-  log.info(
-    `started server on 0.0.0.0:${env.PORT}, ul: http://localhost:${env.PORT}`
-  )
+server.listen(
+  {
+    port: env.PORT,
+    host: "0.0.0.0",
+  },
+  () => log.info(`started server on ${env.PORT}`)
 );
